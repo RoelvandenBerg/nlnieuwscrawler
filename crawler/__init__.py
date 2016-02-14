adf90e67e37f6a617584e81b37f27ba9b206f5ae
RoelvandenBerg <roelvdberg@gmail.com>
2016-02-14 19:58:51 +0100

    - Fixed importing - Fixed filequeue. Filequeue garbage collectible.

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,1 +1,18 @@
 __author__ = 'roelvdberg@gmail.com'
+
+try:
+    import base
+    import crawl
+    import filequeue
+    import model
+    import robot
+    import validate
+    import webpage
+except ImportError:
+    import crawler.crawl as crawl
+    import crawler.base as base
+    import crawler.filequeue as filequeue
+    import crawler.model as model
+    import crawler.robot as robot
+    import crawler.validate as validate
+    import crawler.webpage as webpage
--- crawler/filequeue.py
+++ crawler/filequeue.py
@@ -17,35 +17,46 @@ def _touch(file_name, pickled=False, keep=False):
         os.utime(file_name, None)
 
 
-def _remove(put_queue_name, get_queue_name, _puttable, persistent, pickled,
-            get_pos, initial_get_queue_name, initial_put_queue_name):
-    print('#####   deleting filequeue')
-    if _puttable:
-        if not persistent:
-            os.remove(put_queue_name)
-            os.remove(get_queue_name)
-        else:
-            method = 'ab' if pickled else 'a'
-            with open(put_queue_name, method) as put_queue, \
-                    open(get_queue_name, _file_method('r', pickled)) as f:
-                if pickled:
-                    for _ in range(get_pos):
-                        pickle.load(f)
-                    while True:
-                        try:
-                            pickle.dump(pickle.load(f), put_queue)
-                        except EOFError:
-                            break
-                else:
-                    for _ in range(get_pos):
-                        next(f)
-                    for line in f:
-                        put_queue.write(line.strip('\n') + '\n')
-            _touch(get_queue_name, pickled=pickled)
-            if get_queue_name != initial_get_queue_name or \
-                    put_queue_name != initial_put_queue_name:
-                os.rename(get_queue_name, initial_get_queue_name)
-                os.rename(put_queue_name, initial_put_queue_name)
+def del_file(filename):
+    try:
+        os.remove(filename)
+    except FileNotFoundError:
+        pass
+
+
+def _remove(put_queue_name, get_queue_name, pos_name):
+    del_file(pos_name)
+    del_file(put_queue_name)
+    del_file(get_queue_name)
+
+
+def _remove_persistent(put_queue_name, get_queue_name, pickled, pos_name):
+    try:
+        try:
+            with open(pos_name, 'r') as pn:
+                get_pos = int(pn.read())
+        except FileNotFoundError:
+            get_pos = 0
+        os.remove(pos_name)
+        method = 'ab' if pickled else 'a'
+        with open(put_queue_name, method) as put_queue, \
+                open(get_queue_name, _file_method('r', pickled)) as f:
+            if pickled:
+                for _ in range(get_pos):
+                    pickle.load(f)
+                while True:
+                    try:
+                        pickle.dump(pickle.load(f), put_queue)
+                    except EOFError:
+                        break
+            else:
+                for _ in range(get_pos):
+                    next(f)
+                for line in f:
+                    put_queue.write(line.strip('\n') + '\n')
+        _touch(get_queue_name, pickled=pickled)
+    except (Empty, FileNotFoundError):
+        pass
 
 
 class FileQueueError(Exception):
@@ -71,9 +82,10 @@ class FileQueue(object):
         """
         Low memory FIFO queue that keeps queue on disk.
 
-        Queue is stored in two files:
-        '0_thread_[thread-id or given name]_[id].queue'
-        '1_thread_[thread-id or given name]_[id].queue'
+        Queue is stored in three files:
+        'get_thread_[thread-id or given name]_[id].queue'
+        'put_thread_[thread-id or given name]_[id].queue'
+        'pos_thread_[thread-id or given name]_[id].queue'
 
         The former (read) is used for reading the queue and gets filled with the
         latter (put) when empty. The latter (put) is used to put new items into
@@ -92,50 +104,27 @@ class FileQueue(object):
         :param pickled: uses pickle by default to serialize the items. When
             the items are strings only, pickled can be set to False.
         """
-        self.pickled = pickled
-        if name:
-            self.name_base = name
+        self.fq = _PersistentFileQueue(
+                directory, name, persistent, overwrite, id_, pickled
+            )
+        if persistent:
+            self._finalizer = weakref.finalize(
+                self, _remove_persistent, self.fq.put_queue_name,
+                self.fq.get_queue_name, pickled, self.fq.pos_name
+            )
         else:
-            self.name_base = str(threading.get_ident())
-        self.id = str(id_)
-        self.overwrite = overwrite
-        self.persistent = persistent
-        self.directory = directory.rstrip('/') + '/' if len(directory) else ""
-        if not os.path.exists(self.directory) and self.directory:
-            os.makedirs(self.directory)
-        self.put_queue_name = self._filename('0')
-        self.get_queue_name = self._filename('1')
-        self.initial_put_queue_name = self.put_queue_name
-        self.initial_get_queue_name = self.get_queue_name
-        self.put_lock = threading.Lock()
-        self.get_lock = threading.Lock()
-        self.iterator = iter(self._iterator())
-        self.get_queue_length = 0
-        self.put_queue_length = 0
-        self._puttable = True
-        self.get_pos = 0
-        self._finalizer = weakref.finalize(
-            self, _remove, self.put_queue_name, self.get_queue_name,
-            self._puttable, self.persistent, self.pickled, self.get_pos,
-            self.initial_get_queue_name, self.initial_put_queue_name
-        )
+            self._finalizer = weakref.finalize(
+                self, _remove, self.fq.put_queue_name, self.fq.get_queue_name,
+                self.fq.pos_name
+            )
 
-
-    def put(self, item):
+    def put(self, x):
         """
         Put item into the queue.
 
         :param item: string or other Python object to put in queue.
         """
-        if not self._puttable:
-            raise Empty('Putting to emptied queue is not allowed.')
-        with self.put_lock:
-            with open(self.put_queue_name, self._file_method('a')) as f:
-                if self.pickled:
-                    pickle.dump(item, f)
-                else:
-                    f.write(item + '\n')
-            self.put_queue_length += 1
+        self.fq.put(x)
 
     def get(self):
         """
@@ -146,10 +135,7 @@ class FileQueue(object):
         :return: item (string or other python object) if one is immediately
             available, else raise the Empty exception
         """
-        try:
-            return next(self)
-        except StopIteration:
-            raise Empty('File queue is empty.')
+        return self.fq.get()
 
     def qsize(self):
         """
@@ -160,7 +146,7 @@ class FileQueue(object):
 
         :return: the approximate size of the queue
         """
-        return len(self)
+        return self.fq.qsize()
 
     def empty(self):
         """
@@ -172,13 +158,83 @@ class FileQueue(object):
 
         :return: True if the queue is empty, False otherwise.
         """
-        return len(self) == 0
+        return self.fq.empty()
 
-    # def __del__(self):
-    #     self._finalizer()
-    #
-    # def remove(self):
-    #     self._finalizer()
+    def remove(self):
+        self._finalizer()
+
+    def __len__(self):
+        return len(self.fq)
+
+    def __str__(self):
+        return str(self.fq)
+
+    def __repr__(self):
+        return repr(self.fq)
+
+
+class _PersistentFileQueue(object):
+
+    def __init__(self, directory="", name=None, persistent=False,
+                 overwrite=False, id_=0, pickled=True):
+        self.pickled = pickled
+        self.persistent = persistent
+        if name:
+            self.name_base = name
+        else:
+            self.name_base = str(threading.get_ident())
+        self.id = str(id_)
+        self.overwrite = overwrite
+        self.directory = directory.rstrip('/') + '/' if len(directory) else ""
+        if not os.path.exists(self.directory) and self.directory:
+            os.makedirs(self.directory)
+        self.put_queue_name = self._filename('put')
+        self.get_queue_name = self._filename('get')
+        self.pos_name = self._filename('pos')
+        self._update_pos(0)
+        self.put_lock = threading.Lock()
+        self.get_lock = threading.Lock()
+        self.get_queue_length = 0
+        self.put_queue_length = 0
+        self._puttable = True
+        self.iterator = self._iterator()
+
+    def _update_pos(self, i):
+        self.get_pos = i
+        with open(self.pos_name, 'w') as pn:
+            pn.write(str(self.get_pos))
+
+    def put(self, item):
+        if not self._puttable:
+            raise Empty('Putting to emptied queue is not allowed.')
+        with self.put_lock:
+            with open(self.put_queue_name, self._file_method('a')) as f:
+                if self.pickled:
+                    pickle.dump(item, f)
+                else:
+                    f.write(item + '\n')
+            self.put_queue_length += 1
+
+    def get(self):
+        with self.get_lock:
+            try:
+                get_pos, item = next(self.iterator)
+                if get_pos == 0:
+                    self.get_queue_length = self.put_queue_length
+                    self.put_queue_length = 0
+                else:
+                    self.get_queue_length -= 1
+                self._update_pos(get_pos)
+                return item
+            except StopIteration:
+                self._puttable = False
+                raise Empty('File queue is empty.')
+
+    def qsize(self):
+        return len(self)
+
+    def empty(self):
+        return len(self) == 0
 
     def _filename(self, file_type):
         name = self.directory + file_type + '_thread_' + self.name_base + \
@@ -195,59 +251,44 @@ class FileQueue(object):
     def _file_method(self, method):
         return _file_method(method, self.pickled)
 
-
     def _iterator(self):
-        try_again = True
-        while try_again:
-            with self.get_lock, open(self.get_queue_name,
-                                     self._file_method('r')) as f:
-                if self.pickled:
-                    while True:
-                        try:
-                            self.get_pos += 1
-                            unpickled = pickle.load(f)
-                            self.get_queue_length -= 1
-                            yield unpickled
-                        except EOFError:
-                            break
-                else:
-                    for line in f:
-                        self.get_pos += 1
-                        self.get_queue_length -= 1
-                        yield line.strip('\n')
-            try_again = self._move_strings_to_get_file()
-        os.remove(self.put_queue_name)
-        os.remove(self.get_queue_name)
-        self._puttable = False
-        yield None
+        def iterator(put_queue_name, get_queue_name, pos_name, file_method,
+                     pickled, put_lock):
+            get_pos = 0
+            try_again = 0
+            while try_again < 2:
+                try_again += 1
+                with open(get_queue_name, file_method) as f:
+                    if pickled:
+                        while True:
+                            try:
+                                unpickled = pickle.load(f)
+                            except EOFError:
+                                break
+                            try_again = 0
+                            get_pos += 1
+                            yield get_pos, unpickled
+                    else:
+                        for line in f:
+                            get_pos += 1
+                            yield get_pos, line.strip('\n')
+                            try_again = 0
+                with put_lock:
+                    os.rename(put_queue_name, get_queue_name)
+                    _touch(put_queue_name, pickled=self.pickled)
+                    get_pos = 0
+            os.remove(put_queue_name)
+            os.remove(get_queue_name)
+            os.remove(pos_name)
 
-    def _move_strings_to_get_file(self):
-        try_again = False
-        with self.put_lock, self.get_lock:
-            if self.get_queue_length != 0:
-                if self.persistent:
-                    self.get_queue_length = 0
-                else:
-                    raise FileQueueError('Moving strings to "get" while it is '
-                                         'not completely read. pos = {}, '
-                                         'get_queue_length = {}'.format(
-                        self.get_pos, self.get_queue_length))
-            _touch(self.get_queue_name, pickled=self.pickled)
-            self.get_queue_length = self.put_queue_length
-            self.put_queue_length = 0
-            self.get_pos = 0
-            get_queue_name_old = self.get_queue_name
-            self.get_queue_name = self.put_queue_name
-            self.put_queue_name = get_queue_name_old
-            try_again = bool(self.get_queue_length)
-        return try_again
-
-    def __next__(self):
-        next_ = next(self.iterator)
-        if next_:
-            return next_
-        else:
-            raise StopIteration
+        pos_name = self.pos_name
+        put_queue_name = self.put_queue_name
+        get_queue_name = self.get_queue_name
+        file_method = self._file_method('r')
+        pickled = self.pickled
+        put_lock = self.put_lock
+        return iterator(put_queue_name, get_queue_name, pos_name,
+                        file_method, pickled, put_lock)
 
     def __add__(self, other):
         length = other.get_queue_length
@@ -259,9 +300,6 @@ class FileQueue(object):
             temp.put(string)
         for string in temp:
             other.put(string)
-        return self
-
-    def __iter__(self):
         return self
 
     def __len__(self):
